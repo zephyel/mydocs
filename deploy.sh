73af19d9c38dcb3832568d41d180e032287d8164
zephyel <z2694506351@gmail.com>
2025-06-21 23:57:26 +0800

    test_docs 2025-06-21 23:57:26

--- deploy.sh
+++ deploy.sh
@@ -10,33 +10,34 @@ pnpm docs:build
 echo "✅ 构建完成！"
 
 
-# 在 my-docs里开发和提交源码(main分支)
-# git add .
-# git commit -m "my-docs $(date '+%Y-%m-%d %H:%M:%S')"
-# git push https://github.com/zephyel/zephyel.github.io.git main
-# git push git@github.com:zephyel/zephyel.github.io.git main
+# 推送 test_docs源码到 main分支 (git插件需要)
+echo "📂 推送my-docs源码到main分支..."
+git add -A
+git commit -m "test_docs $(date '+%Y-%m-%d %H:%M:%S')"
+git push https://github.com/zephyel/mydocs.git main                 #使用 HTTPS(需要 token)
+# git push git@github.com:zephyel/mydocs.git main                   #使用 SSH
 
 
-# 进入生成的文件夹，初始化 Git 仓库并提交（注意这里改成了 VuePress 的默认输出目录）
-echo "📂 进入输出目录 dist/, 初始化 Git 仓库并提交更改..."
+# 将 dist目录推送到 gh-pages分支
+echo "📂 推送dist目录到gh-pages分支..."
 cd src/.vuepress/dist
 git init
 git add -A
 git commit -m "deploy $(date '+%Y-%m-%d %H:%M:%S')"
-
-
-# 部署到 GitHub Pages
-echo "🚀 推送到 GitHub Pages..."
 # 如果你想要部署到 https://USERNAME.github.io
-git push -f https://github.com/zephyel/zephyel.github.io.git master
-# git push -f git@github.com:zephyel/zephyel.github.io.git master
-
-# 如果发布到 https:/USERNAME.github.io/<REPO>REPO=github上的项目
-# git push -f git@github.com:USERNAME/<REPO>.git master:gh-pages
+# git push -f https://github.com/zephyel/zephyel.github.io.git master      #使用 HTTPS(需要 token)
+# git push -f git@github.com:zephyel/zephyel.github.io.git master          #使用 SSH
+# 如果发布到 https://USERNAME.github.io/<REPO>REPO=github上的项目
+git push -f https://github.com/zephyel/mydocs.git master:gh-pages          #使用 HTTPS(需要 token)
+# git push -f git@github.com:zephyel/mydocs.git master:gh-pages            #使用 SSH
 
 
 # 返回项目根目录
 cd -
-echo "🏁 部署完成！访问 https://zephyel177.xyz ✨"
+echo "🏁 部署完成！访问 https://zephyel.github.io/mydocs ✨"
+
+
+
+
 
 
